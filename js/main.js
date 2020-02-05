6ab3a37eae3c4513b817859d52631c5baf5d11c1
Jhosuer <jumbojoshua91@gmail.com>
2020-02-05 17:02:14 +0100

    added the whole codebase at once

---
+++ js/main.js
@@ -0,0 +1,63 @@
+let bodyFunction = () => {
+	let navId = document.getElementById('aboutnav');
+	let navId2 = document.getElementById('worknav');
+	let navId3 = document.getElementById('footernav');
+	let link = document.getElementById('link');
+	let link2 = document.getElementById('link2');
+	let link3 = document.getElementById('link3');
+	let link4 = document.getElementById('link4');
+	let navUl = document.getElementById('nav');
+	let clickToNav = document.getElementById('navbtn');
+	navId.addEventListener('click', function(e) {
+		 let url = e.target.href;
+	    let hash = url.substring(url.indexOf("#")+1);
+	    $('html, body').animate({
+	        scrollTop: $('#'+hash).offset().top
+	    }, 800);
+	    return false;
+	});
+
+	navId2.addEventListener('click', function(e) {
+		 let url = e.target.href;
+	    let hash = url.substring(url.indexOf("#")+1);
+	    $('html, body').animate({
+	        scrollTop: $('#'+hash).offset().top
+	    }, 800);
+	    return false;
+	});
+
+	navId3.addEventListener('click', function(e) {
+		 let url = e.target.href;
+	    let hash = url.substring(url.indexOf("#")+1);
+	    $('html, body').animate({
+	        scrollTop: $('#'+hash).offset().top
+	    }, 800);
+	    return false;
+	});
+
+	link.addEventListener('click', function() {
+		// Link to a different page;
+	});
+	link2.addEventListener('click', function() {
+		// Link to a different page;
+	});
+	link3.addEventListener('click', function() {
+		// Link to a different page;
+	});
+	link4.addEventListener('click', function() {
+		// Link to a different page;
+	});
+	clickToNav.addEventListener('click', function() {
+		// if(navUl.style.display == 'block') {
+		// 	navUl.style.display = 'none';
+		// } else {
+		// 	navUl.style.display = 'block';
+		// }
+		if ((window.matchMedia('screen and (max-width: 768px)').matches) && (navUl.style.display == 'block'))   {
+			navUl.style.display = 'none';
+		} else {
+			navUl.style.display = 'block';
+		}
+	});
+}
+bodyFunction();
